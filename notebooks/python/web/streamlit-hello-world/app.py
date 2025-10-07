46b150ec477ebaa93e3b3ce87b5dd1173def6b29
Gabriel Menezes <dmenezes.gabriel@gmail.com>
2025-10-06 21:42:19 -0300

    chore: update notebooks

--- notebooks/python/web/streamlit-hello-world/app.py
+++ notebooks/python/web/streamlit-hello-world/app.py
@@ -12,23 +12,86 @@
 # Application
 
 # %%
-import altair as alt
-import pandas as pd
 import streamlit as st
-from numpy.random import default_rng as rng
 
-df = pd.DataFrame(rng(0).standard_normal((60, 3)), columns=["a", "b", "c"])
+# %% [markdown]
+# Home Page
 
-chart = (
-    alt.Chart(df)
-    .mark_circle()
-    .encode(x="a", y="b", size="c", color="c", tooltip=["a", "b", "c"])
+
+# %%
+def home():
+    st.write("Home")
+
+
+# %% [markdown]
+# Dashboard Page
+
+
+# %%
+def dashboard():
+    st.write("Dashboard")
+
+
+# %% [markdown]
+# Contact Page
+
+
+# %%
+def contact():
+    container = st.container(
+        border=False,
+        height=500,
+        horizontal=True,
+        horizontal_alignment="center",
+        vertical_alignment="center",
+    )
+    form = container.form(
+        key="contact_form",
+        width=400,
+        border=False,
+    )
+
+    header = form.container()
+    header.header("contact")
+
+    body = form.container()
+    body.text_input(label="E-mail")
+    body.text_area(label="Message")
+
+    footer = form.container(
+        horizontal=True,
+        horizontal_alignment="right",
+    )
+    footer.form_submit_button("Submit")
+
+
+# %% [markdown]
+# About Page
+
+
+# %%
+def about():
+    st.write("About")
+
+
+# %% [markdown]
+# Pages configuration
+
+
+# %%
+pg = st.navigation(
+    [
+        st.Page(home, title="Home", icon=":material/house:"),
+        st.Page(dashboard, title="Dashboard", icon=":material/bar_chart:"),
+        st.Page(contact, title="Contact", icon=":material/phone:"),
+        st.Page(about, title="About", icon=":material/person:"),
+    ]
 )
 
-st.altair_chart(chart)
+pg.run()
 
 # %% [markdown]
 # Run application
 # ```sh
-#  uvx --with plotly streamlit run app.py
+#  uvx --with plotly streamlit run app.py --server.runOnSave=True
 # ```
