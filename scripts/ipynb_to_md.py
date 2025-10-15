62dff32d8a53057ae36912c8832592725a95115d
Gabriel Menezes <dmenezes.gabriel@gmail.com>
2025-10-15 09:41:03 -0300

    feat: update mkdocs jupyter plugin

--- scripts/ipynb_to_md.py
+++ scripts/ipynb_to_md.py
@@ -1,11 +1,32 @@
 from pathlib import Path
+from typing import Any, Dict
 
+from markdownify import markdownify as md  # type: ignore
 from nbconvert import MarkdownExporter
 from nbconvert.preprocessors import Preprocessor
 from nbformat.notebooknode import NotebookNode
 
+
+class HtmlOutputToMarkdownProcessor(Preprocessor):
+
+    def preprocess_cell(
+        self, cell: NotebookNode, resources: Dict[str, Any], index: int
+    ) -> tuple[NotebookNode, Dict[str, Any]]:
+        if cell.cell_type == "code" and cell.get("outputs"):
+            for output in cell.outputs:
+                if not ("data" in output and "text/html" in output["data"]):
+                    continue
+                html_content = "".join(output["data"]["text/html"])
+                anchor_tag = "a"
+                markdown_content = md(html_content, strip=[anchor_tag])
+                output["data"]["text/markdown"] = markdown_content
+                del output["data"]["text/html"]
+
+        return cell, resources
+
+
 template = Path("templates/jupyter.md.j2")
-notebook = Path("notebooks/python/data-analysis/seaborn.ipynb")
+notebook = Path("notebooks/python/data-analysis/pandas.ipynb")
 notebook_output = Path("tmp.md")
 import nbformat
 
@@ -13,8 +34,11 @@ import nbformat
 with notebook.open() as f:
     nb = nbformat.read(f, as_version=4)
 
-# Set up MarkdownExporter with template
-exporter = MarkdownExporter(template_file=str(template))
+preprocessors_list = [HtmlOutputToMarkdownProcessor]
+exporter = MarkdownExporter(
+    template_file=str(template),
+    preprocessors=preprocessors_list,
+)
 
 # Export notebook to Markdown
 body, resources = exporter.from_notebook_node(nb)
